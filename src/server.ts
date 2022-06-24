bb6ea8041c04caf7ff82cb97a3f1b2c223d63181
thazsobral <thazsobral@gmail.com>
2022-06-24 20:08:23 -0300

    init: open respository

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express, { Router, Request, Response } from "express";
+
+const app = express();
+const router = Router();
+const PORT = 3333;
+const HOST = "http://localhost";
+
+app.use(express.json());
+
+router.get("/", (request: Request, response: Response) => {
+    response.json({ message: "Hi, I'm Finderplate." });
+})
+
+app.use(router);
+
+app.listen(PORT, () => console.log(`I'm running at ${HOST}:${PORT}.`) )
