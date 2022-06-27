05cdaba112ef31b40665382f45c682b6fc6cb792
thazsobral <thazsobral@gmail.com>
2022-06-26 21:26:27 -0300

    feature: add connection sketch folder

---
+++ src/database/repository.ts
@@ -0,0 +1,35 @@
+import { DataTypes } from "sequelize";
+import sequelize from "./connection";
+
+const Repository = sequelize.define("repository", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    html_url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    author: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    repository_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    stargazers_count: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    forks_count: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    topics: DataTypes.ARRAY(DataTypes.STRING)
+});
+
+export default Repository;
