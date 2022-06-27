ab96de01c8fc3cb9f3de48e1365fe72168af80b7
thazsobral <thazsobral@gmail.com>
2022-06-26 21:28:51 -0300

    feature: addition of bank connection check and sync

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
-import express, { Router, Request, Response } from "express";
+import express, { Router, Request, Response, Application } from "express";
+import sequelize from "./database/connection";
+import Repository from "./database/repository";
 
-const app = express();
+const app: Application = express();
 const router = Router();
 const PORT = 3333;
 const HOST = "http://localhost";
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 router.get("/", (request: Request, response: Response) => {
   response.json({ message: "Hi, I'm Finderplate." });
@@ -13,4 +16,26 @@ router.get("/", (request: Request, response: Response) => {
 
 app.use(router);
 
-app.listen(PORT, () => console.log(`😎 I'm running at ${HOST}:${PORT}.`));
+try {
+  sequelize.authenticate()
+    .then(() => {
+      console.log('Connection has been established successfully.');
+    })
+    .catch((error) => {
+      console.error('Unable to connect to the database:', error);
+    })
+
+  Repository.sync()
+    .then(() => {
+      console.log("All models were synchronized successfully.");
+    })
+    .catch((error) => {
+      console.error(error.message);
+    })
+  app.listen(PORT, () => console.log(`😎 I'm running at ${HOST}:${PORT}.`));  
+} catch (error: any) {
+  
+
+  console.error(`Error ocurred: ${error.message}`)
+}
+
