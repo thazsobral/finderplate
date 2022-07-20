a93884d92c8563b94c367b51be6c7740eac305f0
thazsobral <thazsobral@gmail.com>
2022-07-19 22:55:18 -0300

    initializing project

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,19 @@
-import express, { Application } from "express";
-import cors from "cors";
-import morgan from "morgan";
-// import { User } from "../database/models/user.js";
+import express from 'express';
+const app = express();
+const port = process.env.PORT || 3000;
+import db from './models';
+// import {users} from './seeders/users';
+// import {projects} from './seeders/projects'
+// import {projectassignments} from './seeders/projectassignments'
 
-class App {
-  express: Application;
+app.get('/', (req, res) => {
+    db.Repository.findAll()
+      .then((result: object) => res.json(result)).catch((err: object) => console.error(err));
+})
 
-  constructor() {
-    this.express = express();
-    this.middleware();
-    this.routes();
-  }
 
-  middleware() {
-    this.express.use(cors());
-    this.express.use(express.json());
-    this.express.use(morgan("dev"));
-  }
-
-  routes() {
-    this.express.use("/", (req, res) =>
-      res.json({ msg: "Hi, I'm Finderplate." })
-    );
-    this.express.use("/user", async (req, res) => {
-      // await User.find({});
-    });
-  }
-
-  listen(port: number, cb: () => void) {
-    this.express.listen(port, cb);
-  }
-}
-
-export default new App();
+db.sequelize.sync().then(() => {
+    app.listen(port, () => {
+        console.log(`App listening on port ${port}`)
+    })
+})
