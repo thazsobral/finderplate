9d6dd31675f60d680ca901adac346b7826ae379c
Mario Bischoff <mariobischoffneto@hotmail.com>
2022-09-06 20:20:23 -0300

    fix: change PORT variable

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { db } from './database/db';
 import { router } from './api/routes';
 
 const app = express();
-const port = process.env.API_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(json())
 app.use(router)
