a098609f914e798d21eb8da8a2c21d65676d493c
Mario Bischoff <mariobischoffneto@hotmail.com>
2022-09-06 20:23:15 -0300

    fix: change sensitive variable to env variable

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const url = 'postgres://ylcvybhu:i0zzAuso82pTQ0xiKC3au_fYp8MYHCLT@tiny.db.elephantsql.com/ylcvybhu'
+const url = process.env.DB_URL
 
 export const db = new Sequelize(url, {
     dialect: "postgres"
